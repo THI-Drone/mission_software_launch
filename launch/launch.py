9b79ec1bd466d9cfff582e10342075a917648d12
Philipp66904 <philippg.pgb@gmail.com>
2024-05-23 17:12:01 +0200

    removed names

--- launch/launch.py
+++ launch/launch.py
@@ -2,8 +2,6 @@ from launch import LaunchDescription
 from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
 
-from common_package_py.node_names import NodeNames
-
 def generate_launch_description():
 
     # ROS 2 Parameter configurations for the nodes
@@ -21,27 +19,23 @@ def generate_launch_description():
         Node(
             package='waypoint_package',
             executable='waypoint_node',
-            name=NodeNames.WAYPOINT,
             output='log'
         ),
         Node(
             package='mission_control_package',
             executable='mission_control_node',
-            name=NodeNames.MISSION_CONTROL,
             output='log',
             parameters=[mission_control_params]
         ),
         Node(
             package='qrcode_detection_package',
             executable='qr_code_scanner_node',
-            name=NodeNames.QRCODE_SCANNER,
             output='log',
             parameters=[qr_code_scanner_node_params]
         ),
         Node(
             package='fcc_bridge',
             executable='fcc_bridge',
-            name=NodeNames.FCC_BRIDGE,
             output='log',
             parameters=[fcc_bridge_params]
         )
